95d98d734ee4f955f369502425a4b7071eed8b15
Ahmed Gagan <67264609+adgan11@users.noreply.github.com>
2023-08-22 18:28:23 +0530

    design 1

--- ShoeAppFigmaTut/ContentView.swift
+++ ShoeAppFigmaTut/ContentView.swift
@@ -8,14 +8,99 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var searchText = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        TabView {
+            NavigationStack {
+                ScrollView {
+                    VStack {
+                        Text("Top Brands")
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .font(.system(size: 28))
+                            .padding(.leading, 20)
+                        HStack {
+                            Spacer()
+                                .frame(width: 20)
+                            Image("image6")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 40, height: 40)
+                                .padding()
+                                .background(
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .fill(Color(red: 214/255, green: 214/255, blue: 214/255))
+                                )
+                            Spacer()
+                            Image("image7")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 40, height: 40)
+                                .padding()
+                                .background(
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .fill(Color(red: 214/255, green: 214/255, blue: 214/255))
+                                )
+                            Spacer()
+                            Image("image8")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 40, height: 40)
+                                .padding()
+                                .background(
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .fill(Color(red: 214/255, green: 214/255, blue: 214/255))
+                                )
+                            Spacer()
+                            Image("image9")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 40, height: 40)
+                                .padding()
+                                .background(
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .fill(Color(red: 214/255, green: 214/255, blue: 214/255))
+                                )
+                            Spacer()
+                        }
+                        Text("Top Brands")
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .font(.system(size: 28))
+                            .padding(.leading, 20)
+                        HStack {
+                            VStack {
+                                ShoeCard(shoeImage: "image3", companyImage: "image6", shoePrice: "239.80", shoeName: "Nike Air Max 90")
+                                ShoeCard(shoeImage: "image4", companyImage: "image7", shoePrice: "85.80", shoeName: "Nike Air Max 90")
+                            }
+                            VStack {
+                                Spacer()
+                                    .frame(height: 40)
+                                ShoeCard(shoeImage: "image5", companyImage: "image8", shoePrice: "239.80", shoeName: "Nike Air Max 90")
+                                ShoeCard(shoeImage: "image3", companyImage: "image6", shoePrice: "85.80", shoeName: "Nike Air Max 90")
+                            }
+                        }
+                    }.ignoresSafeArea().navigationBarTitle("Sneakers")
+                        .navigationBarItems(trailing: Image(systemName: "bell.fill")
+                            .resizable().scaledToFit().frame(width: 30, height: 30).opacity(0.3))
+                }
+            }.searchable(text: $searchText)
+                .tabItem() {
+                    Image(systemName: "house.fill")
+                }
+            Text("Likes")
+                .tabItem() {
+                    Image(systemName: "heart.fill")
+                }
+            Text("Cart")
+                .tabItem() {
+                    Image(systemName: "cart.fill")
+                }
+            Text("Account")
+                .tabItem() {
+                    Image(systemName: "person.fill")
+                }
         }
-        .padding()
     }
 }
 
--- ShoeAppFigmaTut/ShoeCard.swift
+++ ShoeAppFigmaTut/ShoeCard.swift
@@ -8,13 +8,43 @@
 import SwiftUI
 
 struct ShoeCard: View {
+    
+    var shoeImage: String
+    var companyImage: String
+    var shoePrice: String
+    var shoeName: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ZStack {
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(Color(red: 214/255, green: 214/255, blue: 214/255))
+                    .frame(width: 150, height: 150)
+                Image(shoeImage)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150, height: 150)
+                    .padding(.all, 10)
+                    .offset(y: -15)
+                Image(companyImage)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 40, height: 40)
+                    .offset(x: 50, y: 50)
+            }
+            Text("$\(shoePrice)")
+                .fontWeight(.bold)
+                .frame(maxWidth: 150, alignment: .leading)
+            Text("$\(shoeName)")
+                .fontWeight(.bold)
+                .opacity(0.3)
+                .frame(maxWidth: 150, alignment: .leading)
+        }
     }
 }
 
 struct ShoeCard_Previews: PreviewProvider {
     static var previews: some View {
-        ShoeCard()
+        ShoeCard(shoeImage: "", companyImage: "", shoePrice: "", shoeName: "")
     }
 }
