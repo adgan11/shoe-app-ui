440727f2baf1d48cb44187879222d724b0c981f4
Ahmed Gagan <67264609+adgan11@users.noreply.github.com>
2023-08-22 16:42:37 +0530

    Initial Commit

---
+++ ShoeAppFigmaTut/ShoeAppFigmaTutApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoeAppFigmaTutApp.swift
+//  ShoeAppFigmaTut
+//
+//  Created by Ahmed Gagan on 22/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ShoeAppFigmaTutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
