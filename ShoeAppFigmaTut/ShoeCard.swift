64eea9c38556b9e59869f31f961399c26ab78594
Ahmed Gagan <67264609+adgan11@users.noreply.github.com>
2023-08-22 18:27:55 +0530

    first commit

---
+++ ShoeAppFigmaTut/ShoeCard.swift
@@ -0,0 +1,20 @@
+//
+//  ShoeCard.swift
+//  ShoeAppFigmaTut
+//
+//  Created by Ahmed Gagan on 22/08/23.
+//
+
+import SwiftUI
+
+struct ShoeCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ShoeCard_Previews: PreviewProvider {
+    static var previews: some View {
+        ShoeCard()
+    }
+}
